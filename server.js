1420b5693ba75bd987982713602ac8ef47492df5
AP696 <addyphilips66@gmail.com>
2022-08-27 23:30:01 +0100

    1

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require("./controllers/image");
 const db = knex({
   client: "pg",
   connection: {
-    host: " postgresql-acute-35473",
-    user: "postgres",
-    password: "Laptop123",
-    database: "smart-brain",
+    host: process.env.DATABASE_URL,
+    ssl: true,
   },
 });
 
